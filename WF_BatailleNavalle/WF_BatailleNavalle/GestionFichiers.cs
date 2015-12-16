3e724d8ef5d904fa536ccfdeb2da96de54603603
LucienCamuglia <lucien.camuglia@gmail.com>
2015-12-16 14:57:15 +0100

    Récupères les joueurs en XML
    Crée le fichier XML selon un fichier de référence

--- WF_BatailleNavalle/WF_BatailleNavalle/GestionFichiers.cs
+++ WF_BatailleNavalle/WF_BatailleNavalle/GestionFichiers.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
@@ -19,6 +20,7 @@ namespace WF_BatailleNavalle
     static class GestionFichiers
     {
         #region champs
+        private const string REGEXJOUEURS = @"P\d;(\w+)";
         private const string LOCK = ".lock";
         private const string EXTENSION = ".bnav";
         private const string CHEMIN = "C:\\BnShare\\";
@@ -45,9 +47,9 @@ namespace WF_BatailleNavalle
         static public void CreeFichier()
         {
             NomFichier = "BN-V01-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
-
-            FileStream fichier = File.Create(CHEMIN + NomFichier + EXTENSION);
-            fichier.Close();
+            //var Source = Properties.Resources.reference;
+            System.IO.File.Copy("./properties/reference.bnav", CHEMIN + NomFichier + EXTENSION, true);
+            //fichier.Close();
 
         }
 
@@ -67,6 +69,87 @@ namespace WF_BatailleNavalle
 
         }
 
+        /// <summary>
+        /// Lecture des joueurs depuis le fichier XML
+        /// </summary>
+        /// <returns> Liste de joueurs </returns>
+        static public List<string> LireJoueurs()
+        {
+            List<String> joueurs = new List<string>();
+            string JoueurTexte = "";
+            XmlDocument document = new XmlDocument();
+            FileStream stream;
+            XmlElement root;
+            XmlNodeList XmlJoueurs;
+            IEnumerator ienum;
+
+            //ouvre le fichier XML
+            stream = File.Open(CHEMIN + NomFichier + EXTENSION, FileMode.Open);
+            //charge le fichier XML
+            document.Load(stream);
+            //Défini le root du fichier XML
+            root = document.DocumentElement;
+            //Défini les élèments a récupèrer
+            XmlJoueurs = root.GetElementsByTagName("JOUEURS");
+            //Récupère toutes les valeurs
+            ienum = XmlJoueurs.GetEnumerator();
+
+            //extrai chaque valeurs aux format texte
+            while (ienum.MoveNext())
+            {
+                XmlNode joueur = (XmlNode)ienum.Current;
+                JoueurTexte += joueur.InnerText;
+            }
+
+            //défini l'expression régulière
+            Regex r = new Regex(REGEXJOUEURS);
+            //extrait les valeurs selon l'expréssion regulière
+            foreach (Match m in r.Matches(JoueurTexte))
+            {
+                joueurs.Add((m.Groups[1]).ToString());
+            }
+
+            return joueurs;
+        }
+
+        static public List<string> LirePlateaux()
+        {
+            List<String> joueurs = new List<string>();
+            string JoueurTexte = "";
+            XmlDocument document = new XmlDocument();
+            FileStream stream;
+            XmlElement root;
+            XmlNodeList XmlJoueurs;
+            IEnumerator ienum;
+
+            //ouvre le fichier XML
+            stream = File.Open(CHEMIN + NomFichier + EXTENSION, FileMode.Open);
+            //charge le fichier XML
+            document.Load(stream);
+            //Défini le root du fichier XML
+            root = document.DocumentElement;
+            //Défini les élèments a récupèrer
+            XmlJoueurs = root.GetElementsByTagName("JOUEURS");
+            //Récupère toutes les valeurs
+            ienum = XmlJoueurs.GetEnumerator();
+
+            //extrai chaque valeurs aux format texte
+            while (ienum.MoveNext())
+            {
+                XmlNode joueur = (XmlNode)ienum.Current;
+                JoueurTexte += joueur.InnerText;
+            }
+
+            //défini l'expression régulière
+            Regex r = new Regex(REGEXJOUEURS);
+            //extrait les valeurs selon l'expréssion regulière
+            foreach (Match m in r.Matches(JoueurTexte))
+            {
+                joueurs.Add((m.Groups[1]).ToString());
+            }
+
+            return joueurs;
+        }
 
 
         /**************************************************PAS FINI****************************************************************************/
@@ -83,43 +166,9 @@ namespace WF_BatailleNavalle
         }
 
 
-        /// <summary>
-        /// Lecture des joueurs depuis le fichier XML
-        /// </summary>
-        /// <returns> Liste de joueurs </returns>
-        static public List<string> LireJoueurs()
-        {
-            List<String> joueurs = new List<string>();
-            try
-            {
-                XmlDocument document = new XmlDocument();
 
-                XmlDeclaration xmldecl;
-                xmldecl = document.CreateXmlDeclaration("1.0", null, null);
-                xmldecl.Encoding = "UTF-8";
-                xmldecl.Standalone = "yes";
 
-                FileStream stream = File.Open(CHEMIN + NomFichier + EXTENSION, FileMode.Open, FileAccess.Read, FileShare.Read);
-                document.Load(stream);
-                XmlElement root = document.DocumentElement;
-                XmlNodeList XmlJoueurs = root.GetElementsByTagName("JOUEURS");
-                IEnumerator ienum = XmlJoueurs.GetEnumerator();
 
-                while (ienum.MoveNext())
-                {
-                    XmlNode joueur = (XmlNode)ienum.Current;
-                    string JoueurTexte = joueur.InnerText;
-                }
-
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-
-            return joueurs;
-
-        }
         #endregion
 
     }
